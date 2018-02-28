d39dacd7bdf7bcf132509ccc91128d0153596585
Slthemf <36306396+Slthemf@users.noreply.github.com>
2018-02-28 12:05:14 -0500

    Add files via upload

---
+++ 38/ComparesTo.java
@@ -0,0 +1,46 @@
+public class ComparesTo {
+	public static void main (String[] args) {
+		System.out.print("Comparing \"admit\" with \"minds\" produces ");
+		int i = "admit".compareTo("minds");
+		System.out.println(i);
+		
+		System.out.print("Comparing \"friendship\" with \"friend\" produces ");
+		System.out.println("friendship".compareTo("friend"));
+		
+		System.out.print("Comparing \" food\" with \"team\"  produces ");
+		System.out.println("food".compareTo("team"));
+		
+		System.out.print("Comparing \"fool\" with \"ace\" produces ");
+		System.out.println("fool".compareTo("ace"));
+		
+		System.out.print("Comparing \"zoo\" with \"game\" produces ");
+		System.out.println("zoo".compareTo("game"));
+		
+		System.out.print("Comparing \"dessert\" with \"desert\" produces ");
+		System.out.println("dessert".compareTo("desert"));
+		
+		System.out.print("Comparing \"biology\" with \"chemistry\" produces ");
+		System.out.println("biology".compareTo("chemistry"));
+		
+		System.out.print("Comparing \"example\" with \"example\" produces ");
+		System.out.println("example".compareTo("example"));
+		
+		System.out.print("Comparing \"spore\" with \"zygote\" produces ");
+		System.out.println("spore".compareTo("zygote"));
+		
+		
+		System.out.print("Comparing \"anti\" with \"antibiotics\" produces");
+		System.out.println("anti".compareTo("antibiotics"));
+		
+		System.out.print("Comparing \"mushroom\" with \"fungi\" produces ");
+		System.out.println("mushroom".compareTo("fungi"));
+		
+		System.out.print("Comparing \"single\" with \"taken\" produces ");
+		System.out.println("single".compareTo("taken"));
+		
+		System.out.print("Comparing \"alcohol\" with \"alcohol\" produces ");
+		System.out.println("alcohol".compareTo("alcohol"));
+	}
+}
+		
+		
